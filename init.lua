1580e6cb0248c97c7d0b40bbe4fce15914a8a743
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-03-28 23:39:49 +1300

    Add neoscroll plugin

--- init.lua
+++ init.lua
@@ -719,5 +719,23 @@ cmp.setup {
   },
 }
 
+require('neoscroll').setup({
+  easing_function = "quadratic"
+})
+
+local t = {}
+-- Syntax: t[keys] = {function, {function arguments}}
+t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '170'}}
+t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '170'}}
+t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '220'}}
+t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '220'}}
+t['<C-y>'] = {'scroll', {'-0.10', 'false', '80'}}
+t['<C-e>'] = {'scroll', { '0.10', 'false', '80'}}
+t['zt']    = {'zt', {'75'}}
+t['zz']    = {'zz', {'90'}}
+t['zb']    = {'zb', {'75'}}
+
+require('neoscroll.config').set_mappings(t)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
