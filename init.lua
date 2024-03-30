efbdd39c55044d7db14ad51044ee85cbfe1fb496
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-03-30 16:32:06 +1300

    Add back in git related plugins

--- init.lua
+++ init.lua
@@ -251,7 +251,13 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
