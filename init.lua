a992e6e34cb137715861fde27648e58a49a76c84
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-05-03 20:53:18 +1200

    Disable treesitter highlighting for tmux files
    It doesn't seem to work well and builtin highlighting does work well.

--- init.lua
+++ init.lua
@@ -962,6 +962,7 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'tmux' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
