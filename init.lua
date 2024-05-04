0a219b1bec5759ec19516033a173b3f7a51348fe
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-05-04 12:37:27 +1200

    Add autoformat-on-save toggle

--- init.lua
+++ init.lua
@@ -743,6 +743,10 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -762,6 +766,56 @@ require('lazy').setup({
         -- javascript = { { "prettierd", "prettier" } },
       },
     },
+    config = function(_, opts)
+      require('conform').setup(opts)
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+
+      vim.api.nvim_create_user_command('FormatToggle', function(args)
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          vim.cmd [[FormatEnable]]
+          vim.cmd [[echo 'Format-on-save enabled']]
+        else
+          if args.bang then
+            vim.cmd [[FormatDisable!]]
+            vim.cmd [[echo 'Format-on-save disabled for current buffer']]
+          else
+            vim.cmd [[FormatDisable]]
+            vim.cmd [[echo 'Format-on-save disabled']]
+          end
+        end
+      end, {
+        desc = 'Toggle autoformat-on-save',
+        bang = true,
+      })
+
+      vim.keymap.set('n', '<leader>tf', function()
+        vim.cmd [[FormatToggle]]
+      end, { desc = '[T]oggle [f]ormat on save' })
+
+      vim.keymap.set('n', '<leader>tF', function()
+        vim.cmd [[FormatToggle!]]
+      end, { desc = '[T]oggle [f]ormat on save (current buffer)' })
+    end,
   },
 
   { -- Autocompletion
