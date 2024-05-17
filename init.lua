9a08e2f874e11ac1d7badbc83d9c6a0595c923d8
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-05-18 00:27:57 +1200

    Add no ignore telescope search variations

--- init.lua
+++ init.lua
@@ -492,14 +492,30 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files { no_ignore = true }
+      end, { desc = '[S]earch [F]iles (no ignore)' })
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' }) -- Alternate keybind
+      vim.keymap.set('n', '<C-S-P>', function()
+        builtin.find_files { no_ignore = true }
+      end, { desc = '[S]earch [F]iles (no ignore)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sW', function()
+        builtin.grep_string { additional_args = { '--no-ignore' } }
+      end, { desc = '[S]earch current [W]ord (no ignore)' })
       vim.keymap.set('v', '<leader>sw', function() -- Allow searching for current selection too
         local text = getVisualSelection()
         builtin.grep_string { search = text }
       end, { desc = '[S]earch currently selected [W]ord(s)' })
+      vim.keymap.set('v', '<leader>sW', function()
+        local text = getVisualSelection()
+        builtin.grep_string { search = text, additional_args = { '--no-ignore' } }
+      end, { desc = '[S]earch currently selected [W]ord(s) (no ignore)' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sG', function()
+        builtin.live_grep { additional_args = { '--no-ignore' } }
+      end, { desc = '[S]earch by [G]rep (no ignore)' })
       vim.keymap.set('n', '<leader>st', builtin.tags, { desc = '[S]earch [T]ags' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
