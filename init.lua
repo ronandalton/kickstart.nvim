57a1f3cc625d7bf7e418ecd2e19e6de47076e2f9
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-05-25 23:31:16 +1200

    Make telescope grep_string match whole words
    Note that this setting doesn't appear to take effect in visual mode.

--- init.lua
+++ init.lua
@@ -500,11 +500,12 @@ require('lazy').setup({
           oldfiles = {
             only_cwd = true,
           },
+          grep_string = {
+            word_match = '-w',
+            additional_args = { '--hidden' },
+          },
           find_files = {
             hidden = true,
-          },
-          grep_string = {
-            additional_args = { '--hidden' },
           },
           live_grep = {
             additional_args = { '--hidden' },
