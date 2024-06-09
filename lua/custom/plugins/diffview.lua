5934b02924b34108b052999da8002ce28f6fa63b
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-06-09 22:14:06 +1200

    Install diffview plugin

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,4 @@
+return {
+  'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose', 'DiffviewFocusFiles', 'DiffviewToggleFiles', 'DiffviewRefresh', 'DiffviewLog' },
+}
