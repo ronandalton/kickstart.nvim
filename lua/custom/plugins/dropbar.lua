b2c8b438efa5c496859e374a6e4852aee301d5ac
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-07-22 23:37:03 +1200

    Add custom treesitter source to dropbar plugin
    The builtin treesitter source isn't very good, so use another plugin to
    provide a better treesitter source. The chosen plugin is nvim-gps. This
    plugin is deprecated, but a fork exists which fixes some issues stopping
    it from working. Note that if the LSP source wasn't wanted as well, then
    dropbar could be replaced by the fgheng/winbar.nvim plugin and a custom
    source wouldn't be needed.

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -1,7 +1,72 @@
+local function get_custom_treesitter_source()
+  local bar = require 'dropbar.bar'
+  return {
+    get_symbols = function(_, _, _)
+      local data = require('nvim-gps').get_data()
+
+      if data == nil then
+        return {}
+      end
+
+      local symbols = {}
+
+      for _, item in ipairs(data) do
+        table.insert(
+          symbols,
+          bar.dropbar_symbol_t:new {
+            -- Note we just highlight each symbol as if it's a function for simplicity
+            icon = item.icon,
+            icon_hl = 'DropBarIconKindFunction',
+            name = item.text,
+            name_hl = 'DropBarKindFunction',
+            on_clock = nil,
+          }
+        )
+      end
+
+      return symbols
+    end,
+  }
+end
+
 return {
   'Bekaboo/dropbar.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
     'nvim-telescope/telescope-fzf-native.nvim',
+    {
+      'ColinKennedy/nvim-gps', -- fork of SmiteshP/nvim-gps (which is archived) with some bug fixes
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+      },
+      opts = {},
+    },
+  },
+  opts = {
+    bar = {
+      sources = function(buf, _)
+        local sources = require 'dropbar.sources'
+        local utils = require 'dropbar.utils'
+        local custom_treesitter_source = get_custom_treesitter_source()
+        if vim.bo[buf].ft == 'markdown' then
+          return {
+            sources.path,
+            sources.markdown,
+          }
+        end
+        if vim.bo[buf].buftype == 'terminal' then
+          return {
+            sources.terminal,
+          }
+        end
+        return {
+          sources.path,
+          utils.source.fallback {
+            sources.lsp,
+            custom_treesitter_source,
+          },
+        }
+      end,
+    },
   },
 }
