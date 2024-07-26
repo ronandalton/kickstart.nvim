e89e1fa722c6d95160c5cfeea00757a22ccdb6b5
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-07-27 00:00:15 +1200

    Specify lazy local dev directory

--- init.lua
+++ init.lua
@@ -1200,6 +1200,9 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
 }, {
+  dev = {
+    path = vim.fn.stdpath 'data' .. '/lazy-local',
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
