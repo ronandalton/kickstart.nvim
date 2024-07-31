ed6879fbca61b9bce2ddecef3c916be0a53d01f3
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-07-31 23:28:49 +1200

    Add workaround for vim-visual-multi and nvim-autopairs conflict

--- lua/custom/plugins/vim-visual-multi.lua
+++ lua/custom/plugins/vim-visual-multi.lua
@@ -1,3 +1,19 @@
 return {
   'mg979/vim-visual-multi',
+  init = function()
+    -- Hack around issue with conflicting insert mode <BS> mapping
+    -- between this plugin and nvim-autopairs
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'visual_multi_start',
+      callback = function()
+        vim.keymap.del('i', '<BS>', { buffer = 0 })
+      end,
+    })
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'visual_multi_exit',
+      callback = function()
+        require('nvim-autopairs').force_attach()
+      end,
+    })
+  end,
 }
