9020d662dcb8969a5dce8619480d41e7b545c7ff
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-08-02 23:12:46 +1200

    Add back in <leader>e diagnostic keymap

--- init.lua
+++ init.lua
@@ -261,6 +261,7 @@ vim.keymap.set({ 'n', 'v' }, '<M-l>', '<c-d>', { remap = true })
 vim.keymap.set({ 'n', 'v' }, '<M-h>', '<c-u>', { remap = true })
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
