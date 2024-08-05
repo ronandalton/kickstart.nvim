ea8b95d23baf6d3e9e78d9460512da940037ee48
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-08-05 21:02:39 +1200

    Fix some issues with vim-visual-multi and re-enable
    Add workarounds for conflicts with other plugins.

--- lua/custom/plugins/vim-visual-multi.lua
+++ lua/custom/plugins/vim-visual-multi.lua
@@ -1,19 +1,31 @@
 return {
   'mg979/vim-visual-multi',
-  cond = false,
   init = function()
+    vim.g.VM_maps = {
+      -- Square braces couldn't be mapped because of some conflict with
+      -- which-key so use curly braces instead
+      ['Goto Next'] = '}',
+      ['Goto Prev'] = '{',
+    }
+
     -- Hack around issue with conflicting insert mode <BS> mapping
     -- between this plugin and nvim-autopairs
     vim.api.nvim_create_autocmd('User', {
       pattern = 'visual_multi_start',
       callback = function()
-        vim.keymap.del('i', '<BS>', { buffer = 0 })
+        pcall(vim.keymap.del, 'i', '<BS>', { buffer = 0 })
+
+        ---@diagnostic disable-next-line: missing-parameter
+        require('lualine').hide()
       end,
     })
     vim.api.nvim_create_autocmd('User', {
       pattern = 'visual_multi_exit',
       callback = function()
         require('nvim-autopairs').force_attach()
+
+        ---@diagnostic disable-next-line: missing-fields
+        require('lualine').hide { unhide = true }
       end,
     })
   end,
