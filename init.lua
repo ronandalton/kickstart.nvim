1cbfcdf07b1475b273d9c1f140c017feeae515be
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-09-16 19:20:19 +1200

    Make diagnostic search keymap only search current buffer
    A keymap is also added for searching diagnostics across all open
    buffers.

--- init.lua
+++ init.lua
@@ -667,7 +667,10 @@ require('lazy').setup({
         builtin.live_grep { additional_args = { '--no-ignore' } }
       end, { desc = '[S]earch by [G]rep (no ignore)' })
       vim.keymap.set('n', '<leader>st', builtin.tags, { desc = '[S]earch [T]ags' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sd', function()
+        builtin.diagnostics { bufnr = 0 }
+      end, { desc = '[S]earch [D]iagnostics (current buffer)' })
+      vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics (all buffers)' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
