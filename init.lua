715ef568ac1fd1a8e03f59da3798971bb64ed4d2
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-09-24 21:48:24 +1200

    Exit visual mode after successful format operation

--- init.lua
+++ init.lua
@@ -983,7 +983,14 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format({ async = true, lsp_fallback = true }, function(err)
+            if not err then
+              local mode = vim.api.nvim_get_mode().mode
+              if vim.startswith(string.lower(mode), 'v') then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+              end
+            end
+          end)
         end,
         mode = '',
         desc = '[F]ormat buffer',
