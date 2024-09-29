b482ccacf2fa051d5ae42686552a7f28a6cdea7e
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-09-29 18:29:58 +1300

    Prefer LSP backend over treesitter for aerial plugin

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -6,6 +6,11 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   opts = {
+    -- Give LSP backend priority over treesitter.
+    -- Treesitter has some problems with C files so use LSP if available.
+    -- https://github.com/stevearc/aerial.nvim/issues/413
+    backends = { 'lsp', 'treesitter', 'markdown', 'asciidoc', 'man' },
+
     disable_max_lines = 20000,
     on_attach = function(bufnr)
       vim.keymap.set('n', '<M-[>', '<cmd>AerialPrev<CR>', { buffer = bufnr })
