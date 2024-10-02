acd3181229203de8c02b147b952d3af045fcb217
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-10-02 20:46:47 +1300

    Add flash plugin and disable mini.surround

---
+++ lua/custom/plugins/flash.lua
@@ -0,0 +1,18 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = {
+        enabled = false,
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
