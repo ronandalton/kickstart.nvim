40f7b14310be02c5c0bef085ca47a7e612f111ef
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-11-10 22:29:53 +1300

    Downgrade indent-blankline
    The older version actually works properly for python code and gets the
    context line position correct.

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -1,14 +1,26 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
+  version = '2.20.7', -- Older version works better (shows proper context) for python
+  main = 'indent_blankline',
   opts = {
-    indent = {
-      char = '▏',
-      tab_char = '▏',
+    char = '▏',
+    context_char = '▏',
+    filetype_exclude = {
+      -- copied from latest version defaults
+      'lspinfo',
+      'packer',
+      'checkhealth',
+      'help',
+      'man',
+      'gitcommit',
+      'TelescopePrompt',
+      'TelescopeResults',
+      '',
     },
-    scope = {
-      show_start = false,
-      show_end = false,
-    },
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = true,
+    show_current_context = true,
+    viewport_buffer = 80,
+    indent_level = 50,
   },
 }
