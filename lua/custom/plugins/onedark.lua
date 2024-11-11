521b274b63298bd80320158887eb3e765161c09e
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2024-11-12 00:22:18 +1300

    Make onedark theme darker
    This is my attempt at making the background darker.

--- lua/custom/plugins/onedark.lua
+++ lua/custom/plugins/onedark.lua
@@ -3,5 +3,10 @@ return {
   priority = 1000,
   opts = {
     style = 'darker',
+    colors = { -- assumes 'darker' style
+      bg0 = '#111315', -- default '#1f2329'
+      bg1 = '#1c2027', -- default '#282c34'
+      bg_d = '#131519', -- default '#181b20'
+    },
   },
 }
