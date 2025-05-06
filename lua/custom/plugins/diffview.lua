a496ccb8149709f74130e6c521c3f46a6284c1db
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2025-05-06 20:14:22 +1200

    Change diffview default merge tool layout to plain

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,4 +2,11 @@ return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose', 'DiffviewFocusFiles', 'DiffviewToggleFiles', 'DiffviewRefresh', 'DiffviewLog' },
   keys = { { '<leader>od', '<cmd>DiffviewOpen<cr>', desc = '[O]pen [D]iff View' } },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = 'diff1_plain',
+      },
+    },
+  },
 }
