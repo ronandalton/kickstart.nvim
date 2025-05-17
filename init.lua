0543a8020004f3968481023ec0f48f0bab134c0d
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2025-05-18 00:11:29 +1200

    Fix cursor resuming in wrong place in git blames

--- init.lua
+++ init.lua
@@ -530,7 +530,7 @@ vim.cmd [[
     autocmd BufRead * autocmd FileType <buffer> ++once
       \ let s:line = line("'\"")
       \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
-      \      && index(['xxd', 'gitrebase'], &filetype) == -1
+      \      && index(['xxd', 'gitrebase', 'git'], &filetype) == -1
       \ |   execute "normal! g`\""
       \ | endif
   augroup END
